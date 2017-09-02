37fbd626050d0397346508ce95520c87f5477063
yanika28 <58160381@go.buu.ac.th>
2017-09-02 15:50:09 +0700

    tennis-game03.spec.js

--- tennis-game03.spec.js
+++ tennis-game03.spec.js
@@ -1,19 +1,41 @@
-function TennisGame(){
-  this.reset = () => {
+function TennisGame() {
+    const scoreString = ['Love', 'Fifteen', 'Thirty', 'Forthy']
+    this.playerBScore = 0
 
-  }
-  this.echo = () => {
-    return 'Love - Fifteen'
-  }
+    this.reset = () => {
+        this.playerBScore = 0
+    }
+
+    this.echo = () => {
+       return `Love - ${scoreString[this.playerBScore]}`
+    }
+
+    this.playerBGetScore = () => {
+        this.playerBScore++
+    }
 }
 
-test('Echo "Love - Fifteen" when playerB get first score', () => {
-//Arrange
-  let app = new TennisGame()
-//Act
-  app.reset()
-  let result = app.echo()
+test('Echo "Love - Love" when game start', () => {
+    // Arrange
+    let app = new TennisGame()
 
-//Assert
-  expect(result).toBe('Love - Fifteen')
+    // Act
+    app.reset()
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Love')
+})
+
+test('Echo "Love - Fifteen" when playerB get first score', () => {
+    // Arrange
+    let app = new TennisGame()
+    app.reset()
+    app.playerBGetScore()
+
+    // Act
+    let result = app.echo()
+
+    // Assert
+    expect(result).toBe('Love - Fifteen')
 })
